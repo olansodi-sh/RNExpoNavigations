d3f7f0bd70231d66082b823de809ac93fa479ba2
DevWhoCares <oliversolano2001@gmail.com>
2025-01-22 22:31:54 -0500

    se realiza configuración de estrucutra para los colores

--- app/index.tsx
+++ app/index.tsx
@@ -5,9 +5,9 @@ const index = () => {
   return (
     <SafeAreaView>
         <View className='mt-6 mx-2.5'>
-          <Text className='text-3xl font-work-black'>font-work-black</Text>
-          <Text className='text-3xl font-work-medium'>font-work-medium</Text>
-          <Text className='text-3xl font-work-light'>font-work-light</Text>
+          <Text className='text-3xl font-work-black text-primary'>font-work-black</Text>
+          <Text className='text-3xl font-work-medium text-secondary-200'>font-work-medium</Text>
+          <Text className='text-3xl font-work-light text-tertiary'>font-work-light</Text>
         </View>
     </SafeAreaView>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+      colors:{
+        primary:'#49129C',
+        secondary:{
+          DEFAULT:'#B40086',
+          100:'#C51297',
+          200:'#831266',
+        },
+        tertiary:'#EF2967',
+      },
       fontFamily:{
         'work-black': ['WorkSans-Black','sans-serif'],
         'work-light': ['WorkSans-Light','sans-serif'],
