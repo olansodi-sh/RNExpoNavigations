606b05d52ed7fbfbfd99775b54b6ac7b20b7cc92
DevWhoCares <oliversolano2001@gmail.com>
2025-01-23 00:21:58 -0500

    Se realiza personalización del StackNavigation

--- app/(stack)/_layout.tsx
+++ app/(stack)/_layout.tsx
@@ -4,9 +4,15 @@ import { Stack } from 'expo-router'
 
 const StackLayout = () => {
   return (
-    <Stack>
+    <Stack
+        screenOptions={{
+            headerShown: true,
+            headerShadowVisible: false,
+            contentStyle: { backgroundColor: 'white' },
+        }}
+    >
         <Stack.Screen name='home/index' options={{ title:'Home Screen'}}/>
-        <Stack.Screen name='products/index' options={{ title:'Products Screen'}}/>
+        <Stack.Screen name='products/index' options={{ title:'Products Screen', animation:'ios_from_right'}}/>
         <Stack.Screen name='profile/index' options={{ title:'Profile Screen'}}/>
         <Stack.Screen name='settings/index' options={{ title:'Settings Screen'}}/>
     </Stack>
